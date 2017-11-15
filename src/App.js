23e98afc158ca6a53fbe7a8e4d31d18cd79ae7e5
Dung Huynh <clock.quangdung@gmail.com>
2017-11-15 22:02:05 +0700

    setup Redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import uuid from 'uuid/v4';
 import logo from './logo.svg';
 import './styles/App.css';
-import firebase from './firebase';
+import firebase from './firebase/firebase';
 
 class App extends Component {
   constructor(props) {
@@ -12,7 +13,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    const id = Math.floor(Math.random() * 10000000);
+    const id = uuid();
     const ref = firebase.database().ref(`addresses/${id}`);
     ref.set({
       city: 'Ho Chi Minh',
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import address from './address.reducer';
+
+export default combineReducers({
+  address,
+});
