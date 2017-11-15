09368ea964fc9cd6d7ebbbaf062b0184da73640f
dzuncoi <clock.quangdung@gmail.com>
2017-11-15 16:25:40 +0700

    setup eslint

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import logo from './logo.svg';
 import './styles/App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      header: 'Welcome to React',
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">{this.state.header}</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
